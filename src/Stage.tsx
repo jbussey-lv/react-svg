6b883e3b4954283b2d563b4782d2039265a02def
John Bussey <jbussey365@yahoo.com>
2022-08-07 01:55:33 -0400

    'figured out fixed with stroke'

--- src/Stage.tsx
+++ src/Stage.tsx
@@ -7,24 +7,23 @@ export default class Stage {
   private pixelHeight: number;
   private originX: number;
   private originY: number;
-  public scale: number;
-  public pixelsPerMeter: number = 4;
+  public pixelsPerMeter: number;
   public sprites: Sprite[] = [];
 
-  constructor(pixelWidth: number, pixelHeight: number, originX: number, originY: number, scale: number) {
+  constructor(pixelWidth: number, pixelHeight: number, originX: number, originY: number, pixelsPerMeter: number) {
     this.pixelWidth = pixelWidth;
     this.pixelHeight = pixelHeight;
     this.originX = originX;
     this.originY = originY;
-    this.scale = scale;
+    this.pixelsPerMeter = pixelsPerMeter;
   }
 
   render(): JSX.Element {
 
-    let viewHeight = this.pixelHeight/this.scale;
+    let viewHeight = this.pixelHeight/this.pixelsPerMeter;
 
-    let viewBox = `${this.originX} ${this.originY} ${this.pixelWidth/this.scale} ${this.pixelHeight/this.scale}`;
-    let transform = `translate(0, ${viewHeight + 2* this.originY}) scale(1,-1)`;
+    let viewBox = `${this.originX} ${this.originY} ${this.pixelWidth/this.pixelsPerMeter} ${this.pixelHeight/this.pixelsPerMeter}`;
+    let transform = `translate(0, ${viewHeight + 2 * this.originY}) scale(1,-1)`;
     return (
       <svg viewBox={viewBox} width={this.pixelWidth} height={this.pixelHeight} style={{border: "1px solid black"}}>
         <g transform={transform}>
--- src/index.tsx
+++ src/index.tsx
@@ -37,28 +37,28 @@ class House extends Sprite {
   }
 
   render(): JSX.Element {
-    let transform = `translate(${this.x}, ${this.y}) rotate(5, 100, 0)`;
+    let transform = `translate(${this.x}, ${this.y}) rotate(5, 1, 0)`;
 
     return (
-      <path d="M 0 0 L 200 0 L 200 200 L 100 300 L 0 200 z"
+      <path d="M 0 0 L 2 0 L 2 2 L 1 3 L 0 2 z"
             fill="red" 
             stroke="blue" 
-            stroke-width="3" 
-            transform={transform}
-            />
+            stroke-width="2"
+            vector-effect="non-scaling-stroke"
+            transform={transform}/>
     )
   }
 }
 
-let stage = new Stage(1000, 600, -300, -100, 1.5);
+let stage = new Stage(1000, 600, -2, -1, 30);
 stage.sprites = [
-  new House(50, 100),
-  new Ball(0, 0, 5),
-  new Ball(0, 100, 5),
-  new Ball(100, 0, 5),
-  new Ball(100, 100, 5),
-  new Ball(200, 0, 5),
-  new Ball(200, 100, 5)
+  new House(1, 1),
+  new Ball(0, 0, 0.1),
+  new Ball(0, 1, 0.1),
+  new Ball(1, 0, 0.1),
+  new Ball(1, 1, 0.1),
+  new Ball(2, 0, 0.1),
+  new Ball(2, 1, 0.1)
 ]
 
 ReactDOM.render(
